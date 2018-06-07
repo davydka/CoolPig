d1ec388765959aafbba9c43ed521ea143f8bbe78
davydka <davydka@gmail.com>
2018-06-07 12:16:42 -0400

    Saving an old project exploring opencv.

---
+++ CoolPig/ViewController.m
@@ -0,0 +1,192 @@
+//
+//  ViewController.m
+//  CoolPig
+//
+//  Created by Joseph Howse on 2015-11-03.
+//  Copyright © 2015 Nummist Media Corporation Limited. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//  (1) Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//  (2) Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//  (3) Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import <opencv2/core.hpp>
+#import <opencv2/imgcodecs/ios.h>
+#import <opencv2/imgproc.hpp>
+#import <opencv2/aruco.hpp>
+
+#ifdef WITH_OPENCV_CONTRIB
+#import <opencv2/xphoto.hpp>
+#endif
+
+#import "ViewController.h"
+#import "VideoCamera.h"
+
+
+#define RAND_0_1() ((double)arc4random() / 0x100000000)
+
+
+@interface ViewController () <CvVideoCameraDelegate> {
+    cv::Mat originalMat;
+    cv::Mat updatedMat;
+}
+
+@property IBOutlet UIView *backgroundView;
+@property IBOutlet UIImageView *imageView;
+@property NSTimer *timer;
+
+@property VideoCamera *videoCamera;
+- (void)refresh;
+- (void)processImage:(cv::Mat &)image;
+
+- (void)updateImage;
+
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Load a UIImage from a resource file.
+    UIImage *originalImage = [UIImage imageNamed:@"Piggy.png"];
+    
+    // Convert the UIImage to a cv::Mat.
+    UIImageToMat(originalImage, originalMat);
+    
+    switch (originalMat.type()) {
+        case CV_8UC1:
+            // The cv::Mat is in grayscale format.
+            // Convert it to RGB format.
+            cv::cvtColor(originalMat, originalMat, cv::COLOR_GRAY2RGB);
+            break;
+        case CV_8UC4: {
+            // The cv::Mat is in RGBA format.
+            // Convert it to RGB format.
+            cv::cvtColor(originalMat, originalMat, cv::COLOR_RGBA2RGB);
+#ifdef WITH_OPENCV_CONTRIB
+            // Adjust the white balance.
+            cv::Ptr<cv::xphoto::GrayworldWB> whiteBalancer = cv::xphoto::createGrayworldWB();
+            whiteBalancer->balanceWhite(originalMat, originalMat);
+#endif
+            break;
+        }
+        case CV_8UC3: {
+            // The cv::Mat is in RGB format.
+#ifdef WITH_OPENCV_CONTRIB
+            // Adjust the white balance.
+            cv::Ptr<cv::xphoto::GrayworldWB> whiteBalancer = cv::xphoto::createGrayworldWB();
+            whiteBalancer->balanceWhite(originalMat, originalMat);
+#endif
+            break;
+        }
+        default:
+            break;
+    }
+    
+    // Call an update method every 2 seconds.
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(updateImage) userInfo:nil repeats:YES];
+    
+    // Get a notification center and queue.
+    // These will provide notifications about application lifecycle events.
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    NSOperationQueue *queue = [NSOperationQueue mainQueue];
+    
+    // When the application enters the background, stop the update timer.
+    [notificationCenter addObserverForName:UIApplicationDidEnterBackgroundNotification object:nil queue:queue usingBlock:^(NSNotification *note) {
+        [self.timer invalidate];
+        self.timer = nil;
+    }];
+    
+    // When the application re-enters the foreground, restart the update timer.
+    [notificationCenter addObserverForName:UIApplicationWillEnterForegroundNotification object:nil queue:queue usingBlock:^(NSNotification *note) {
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(updateImage) userInfo:nil repeats:YES];
+    }];
+    
+    self.videoCamera = [[VideoCamera alloc] initWithParentView:self.backgroundView];
+    self.videoCamera.delegate = self;
+    self.videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPresetHigh;
+    self.videoCamera.defaultFPS = 30;
+    self.videoCamera.letterboxPreview = YES;
+    self.videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionFront;
+    
+    //cv::Ptr<cv::aruco::DetectorParameters>;
+}
+
+
+- (void)updateImage {
+    // Generate a random color.
+    double r = 0.5 + RAND_0_1() * 1.0;
+    double g = 0.6 + RAND_0_1() * 0.8;
+    double b = 0.4 + RAND_0_1() * 1.2;
+    cv::Scalar randomColor(r, g, b);
+    
+    // Create an updated, tinted cv::Mat by multiplying the original cv::Mat and the random color.
+    cv::multiply(originalMat, randomColor, updatedMat);
+    
+    // Convert the updated cv::Mat to a UIImage and display it in the UIImageView.
+    
+    
+    self.imageView.image = MatToUIImage(updatedMat);
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    switch ([UIDevice currentDevice].orientation) {
+        case UIDeviceOrientationPortraitUpsideDown:
+            self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortraitUpsideDown;
+            break;
+        case UIDeviceOrientationLandscapeLeft:
+            self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationLandscapeLeft;
+            break;
+        case UIDeviceOrientationLandscapeRight:
+            self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationLandscapeRight;
+            break;
+        default:
+            self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
+            break;
+    }
+    
+    [self refresh];
+}
+
+
+- (void)processImage:(cv::Mat &)image {
+    cv::Mat image_copy;
+    cvtColor(image, image_copy, CV_BGRA2BGR);
+    
+    // invert image
+    bitwise_not(image_copy, image_copy);
+    cvtColor(image_copy, image, CV_BGR2BGRA);
+}
+
+- (void)refresh {
+    // Start or restart the video.
+    [self.videoCamera stop];
+    [self.videoCamera start];
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,5 @@
+# Cool Pig
+
+Just playing around with opencv libs in iOS.
+
+Donwload, unzip, and add opencv2.framework.zip to the root of the project.
